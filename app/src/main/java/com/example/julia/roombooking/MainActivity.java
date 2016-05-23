4bba2f517236a6dd539dea3dbfeaf6b536f45132
julget <julget22@gmail.com>
2016-05-24 01:36:29 +0200

    julia

--- app/src/main/java/com/example/julia/roombooking/MainActivity.java
+++ app/src/main/java/com/example/julia/roombooking/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.julia.roombooking;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,18 +12,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         android.support.v7.app.ActionBar ab = getSupportActionBar();
-        ab.setLogo(R.drawable.common_ic_googleplayservices);
+        ab.setLogo(R.mipmap.uit_logo);
         ab.setDisplayShowHomeEnabled(true);
         ab.setDisplayUseLogoEnabled(true);
 
-
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.instillinger:
+
+                break;
+            case R.id.login:
+
+                break;
+            case R.id.ins:
+                break;
+            case R.id.hjelp:
+                break;
+            case R.id.avslutt:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
+
